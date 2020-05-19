9b3925ca258ff4e7183c7519895e4ebc7a74bba1
Steven Ametjan <steve@hittingthebottle.com>
2020-05-18 22:18:37 -0700

    Remove customized variables from top of config
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- emacs/init.el
+++ emacs/init.el
@@ -4,9 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(misterioso))
- '(package-selected-packages '(forge magit diminish projectile which-key use-package))
- '(projectile-mode t nil (projectile)))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
