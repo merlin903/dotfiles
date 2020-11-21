d9a0820034f491c045a480339b933095c45648cb
Steven Ametjan <steve@hittingthebottle.com>
2020-11-20 21:41:42 -0800

    Setup pyenv and pipx in Fish

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+pyenv init - | source
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
+
+# Created by `userpath` on 2020-11-21 05:36:24
+set PATH $PATH /Users/sametjan/.local/bin
+register-python-argcomplete --shell fish pipx | source
